af91e7067d39f35bcfd99227eb4a242fb0a75af4
gagankumar8294 <gagankumar8294@gmail.com>
2026-03-09 22:45:25 +0530

    cretaed dummmy data and rendered on the screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { useState } from "react";
+
 function App() {
+
+  const [students] = useState([
+    { id: 1, name: "Gagan", email: "gagan@gmail.com", age: 23 },
+    { id: 2, name: "Ravi", email: "ravi@gmail.com", age: 24 },
+    { id: 3, name: "Arjun", email: "arjun@gmail.com", age: 22 }
+  ]);
+
   return (
-    <>
-    <h1>Component buy</h1>
-    </>
-  )
+    <div>
+      <h1>Students Table</h1>
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {students.map((student) => (
+            <tr key={student.id}>
+              <td>{student.name}</td>
+              <td>{student.email}</td>
+              <td>{student.age}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
